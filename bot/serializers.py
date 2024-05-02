e4366c58aaedde65f28afe3f1326792e78984182
Bharat Thakur <bharat.thakur@joshtechnologygroup.com>
2024-05-03 04:00:34 +0530

    Added upload doc api

--- bot/serializers.py
+++ bot/serializers.py
@@ -1,7 +1,25 @@
 # serializers.py
-from rest_framework import serializers
+from rest_framework import serializers as rest_serializers
+from bot import utils as bot_utils
 
-class QuestionResponseSerializer(serializers.Serializer):
+
+class QuestionResponseSerializer(rest_serializers.Serializer):
     "Serializer to validate given input, create/store embeddings & fetch result of question"
-    question = serializers.CharField(max_length=1000)
-    project_id = serializers.IntegerField()
+    question = rest_serializers.CharField(max_length=1000)
+    project_id = rest_serializers.IntegerField()
+
+
+class UploadDocSerializer(rest_serializers.Serializer):
+    data = rest_serializers.FileField(write_only=True)
+    file_name = rest_serializers.CharField()
+    project = rest_serializers  .CharField()
+
+    def save(self):
+        content = self.validated_data['data'].open('r').read()
+        pymongo_driver = bot_utils.PyMongoDriver()
+        pymongo_driver.create_vector_document(
+            content,
+            bot_utils.OpenAiService.generate_embeddings(content),
+            self.validated_data['file_name'],
+            self.validated_data['project']
+        )
